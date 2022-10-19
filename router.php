0c855b946daad4a0263ade2baa6d9347b560598b
Robin Couet <couetrobin@gmail.com>
2022-10-19 12:26:53 +0200

    simplification

--- router.php
+++ router.php
@@ -3,8 +3,6 @@ require('config/index.php');
 require(DIR_MODEL . '/database.php');
 require(DIR_CONTROLLER . "BedController.php");
 
-$params = $_GET['params'] ?? '';
-$params = explode("/", $params);
-$method = empty($params[0]) ? 'index' : $params[0];
+$method = empty($_GET['params']) ? 'index' : $_GET['params'];
 $page = new BedController();
 $page->$method();
